8221c17ff2b23670a3b5c47d8827e36d0c113779
Mike Trinkala <trink@acm.org>
2013-09-25 14:20:50 -0700

    Patch rapidjson double to string conversion

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -4,10 +4,15 @@
 
 include(ExternalProject)
 set_property(DIRECTORY PROPERTY EP_BASE "${CMAKE_BINARY_DIR}/ep_base")
+find_program(PATCH_EXECUTABLE patch)
+if (NOT PATCH_EXECUTABLE)
+   message(FATAL_ERROR "patch not found")
+endif()
 
 externalproject_add(
     rapidjson
     SVN_REPOSITORY http://rapidjson.googlecode.com/svn/trunk/
+    PATCH_COMMAND ${PATCH_EXECUTABLE} -p1 < ${CMAKE_CURRENT_LIST_DIR}/rapidjson.patch
     BUILD_COMMAND ""
     CONFIGURE_COMMAND ""
     INSTALL_COMMAND ""
